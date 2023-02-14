2f9b8cf663a5530e198c118851d240bb30bfca40
Victor Sattamini <victor.l.sattamini@gmail.com>
2023-02-14 13:57:46 -0300

    commiting basic app function

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ def index():
             model="text-davinci-003",
             prompt=generate_prompt(animal),
             temperature=0.6,
+            frequency_penalty=0.05,
+            max_tokens=1500,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -23,13 +25,16 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+    return """Write a good prompt for an artificial intelligence system that creates images from text (Midjourney). The image is the cover for an article about {}.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
+Here are three typical prompts:
+
+1. "A vast sky with various glowing strings of memories shaping an enormous aurora borealis like glowing foliage, white, orange, extremely beautiful, beautiful universe twist, beautiful nightfall, cinematic lighting, beautiful field, high definition, high quality, hyper detail."
+
+2. "Kneeling cat knight, portrait, finely detailed armor, intricate design, silver, silk, cinematic lighting, 4k."
+
+3. "Ultra sharp award winning underwater nature photography of a woman riding a glistening gradient sea horse, backlit, depth of field, ocean floor, lush vegetation, particles, solar rays, coral, golden fishes, under water fashion photography, woman riding a seahorse, ultra sharp award winning photography."
+
+You can use different words or concepts. Write just one prompt.""".format(
         animal.capitalize()
     )
